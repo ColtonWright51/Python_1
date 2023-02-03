977133793fb5b97e08e94342e08a0ee91f18399e
ColtonWright51 <cw717317@ohio.edu>
2023-02-03 13:13:36 -0500

    Add heatrod

---
+++ src/ME6570/sim_heat_rod.py
@@ -0,0 +1,34 @@
+"""
+Created by Colton Wright on 2/3/2023
+Adapted from John Cotton's MATLAB code created in 2008
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_conn(n_elements):
+    iconn = np.zeros(n_elements)
+
+def get_kgl():
+    print("get_kgl")
+
+def get_load():
+    print("get_load")
+
+def solve_heat_rod():
+    print("solve_heat_rod")
+
+def main():
+    
+    # CONSTANTS
+    k = 0.2
+    alpha = 0.4
+    f1 = 1
+    tE = 20
+    len = 1
+    nnod = 8
+    (x, u) = solve_heat_rod(k, alpha, f1, tE, len, nnod)
+
+if __name__ == '__main__':
+    main()
