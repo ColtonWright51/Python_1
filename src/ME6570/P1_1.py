1c1d7b619801a72f4ffaaf76195b490846d084bc
ColtonWright51 <cw717317@ohio.edu>
2023-02-26 23:16:20 -0500

    Playing w ME6570 Project1

---
+++ src/ME6570/P1_1.py
@@ -0,0 +1,48 @@
+"""
+P1_1.py
+Created by Colton Wright on 2/26/2023
+
+First program for John Cotton's Project 1 in ME6570
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import sympy
+from sympy import symbols
+import os
+import sys
+import hw3
+np.set_printoptions(linewidth=sys.maxsize,threshold=sys.maxsize, precision=16)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", "ME6570", "P1_1")
+os.makedirs(IMAGES_PATH, exist_ok=True)
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+#------------------------------------------------------------------------------
+
+class ApproxODE:
+    K = np.zeros((10))
+
+
+
+
+
+q, k, x, L, c_bar = symbols("q k x L c_bar")
+c = sympy.Function("c")(x)
+dcdx = sympy.diff(c, x)
+
+eq1 = sympy.Eq(q,  -k*sympy.diff(c, x))
+eq2 = sympy.Eq(c(L), c_bar)
+
+eqs = [eq1, eq2]
+solution = sympy.solvers.ode.dsolve(eq1)
+sympy.pprint(solution)
+
