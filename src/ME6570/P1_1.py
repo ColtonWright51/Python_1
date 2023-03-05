16a52b98ecc888d0855fe66aac76dc8dab64fc73
ColtonWright51 <cw717317@ohio.edu>
2023-03-05 18:18:28 -0500

    Building class

--- src/ME6570/P1_1.py
+++ src/ME6570/P1_1.py
@@ -1,5 +1,6 @@
 """
 P1_1.py
+
 Created by Colton Wright on 2/26/2023
 
 First program for John Cotton's Project 1 in ME6570
@@ -13,8 +14,10 @@ from sympy import symbols
 import os
 import sys
 import hw3
+import time
 np.set_printoptions(linewidth=sys.maxsize,threshold=sys.maxsize, precision=16)
 
+start_timer = time.time()
 # Where to save the figures
 PROJECT_ROOT_DIR = "."
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", "ME6570", "P1_1")
@@ -28,27 +31,44 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
 
 #------------------------------------------------------------------------------
 
+
 class ApproxODE:
 
-    def __init__(self):
-            print("Hi")
+
+    def __init__(self, A, k, Q, L, c_bar, q_bar, n_nodes):
+        self.A = A
+        self.k = k
+        self.Q = Q
+        self.L = L
+        self.c_bar = c_bar
+        self.q_bar = q_bar
+        self.n_nodes = n_nodes
 
 
+    def solve_diffusion(self):
+        self.h_e = self.L/self.n_nodes
+        self.K = self.get_kgl()
+
+
+    def get_kgl(self):
+        print()
+
+    
 
 k= 0.1 # Diffusion coefficient [m^2 s^-1]
 q = 0.1 # Diffusion flux [kg m^-2 s^-1]
 L = 2 # Length of rod
 c1 = 5
 x = np.linspace(0,L,1000)
-
-
-
 c = -k*q*x+c1 # Concentration [kg m^-2]
-
 plt.figure()
 plt.plot(x, c)
 plt.grid(True)
 
 
+Approx1 = ApproxODE(.5, .1, .1, 1, 1, 1, 2)
+
+
+print("Runtime:", time.time()-start_timer)
 
 plt.show()
